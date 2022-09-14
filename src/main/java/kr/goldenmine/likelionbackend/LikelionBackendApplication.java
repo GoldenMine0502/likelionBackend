955181edea5ff57c4f4dae8fd7aad3740e61e8e9
GoldenMine0502 <ehe123@naver.com>
2022-09-15 07:30:13 +0900

    그냥 사이트 보고 따라친건데 안되더라

---
+++ src/main/java/kr/goldenmine/likelionbackend/LikelionBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.goldenmine.likelionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/goldenmine/likelionbackend/LikelionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.goldenmine.likelionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
